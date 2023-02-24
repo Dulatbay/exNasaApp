42cfe7cb43f67e0b8d5e83833a12a886f5cb73bb
Akhan Dulatbay <adulatbai@bk.ru>
2023-02-24 18:24:42 +0600

    1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Services\NasaApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,11 +13,6 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class HomeController extends AbstractController
 {
-
-    /**
-     * @throws TransportExceptionInterface
-     */
-
     #[Route("/")]
     public function welcome(): Response
     {
@@ -26,8 +22,7 @@ class HomeController extends AbstractController
     #[Route('/homepage')]
     public function homepage(NasaApi $api): Response
     {
-        $apods = $api->getPictureOfDay();
-        $roverPhotosTmp = (array_slice(array($api->getMarsPhotos()), 0, 3));
-        return $this->render('base.html.twig',  ['apods'=>$apods]);
+        return $this->render("base.html.twig");
+
     }
 }
--- src/Services/NasaApi.php
+++ src/Services/NasaApi.php
@@ -4,13 +4,17 @@ namespace App\Services;
 
 
 use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class NasaApi
 {
     private HttpClientInterface $httpClient;
-    private string $api_key = 'vypQbfVJX694pYGvYeylkvdU7BcrZPmVg0n1mYfD';
+    // private string $api_key = 'vypQbfVJX694pYGvYeylkvdU7BcrZPmVg0n1mYfD';
+       private string $api_key = 'DEMO_KEY';
 
     public function __construct(HttpClientInterface $httpClient)
     {
@@ -19,53 +23,31 @@ class NasaApi
 
     /**
      * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
      */
     public function getPictureOfDay(int $count = 3)
     {
         // TODO: remake this code
         $response = $this->httpClient->request(
             'GET',
-            'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=' . $count
+            'https://api.nasa.gov/planetary/apod?count=' . $count . '&api_key=' . $this->api_key
         );
         $statusCode = $response->getStatusCode();
-        $content = json_decode($response->getContent());
-        return $content;
+        return json_decode($response->getContent());
 
     }
 
-    public function getMarsPhotos(int $page = 1){
-        // TODO
-        $response = $this->httpClient->request(
-            'GET',
-            'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY&page=' . $page
-        );
-        /*
-
-        response model =>
-        0 => {
-            +"id": 102685
-            +"sol": 1004
-            +"camera": {
-                +"id": 20
-                +"name": "FHAZ"
-                +"rover_id": 5
-                +"full_name": "Front Hazard Avoidance Camera"
-            }
-            +"img_src": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01004/opgs/edr/fcam/FLB_486615455EDR_F0481570FHAZ00323M_.JPG"
-            +"earth_date": "2015-06-03"
-            +"rover": {#493 ▼
-                +"id": 5
-                +"name": "Curiosity"
-                +"landing_date": "2012-08-06"
-                +"launch_date": "2011-11-26"
-                +"status": "active"
-            }
-        }
-
-
-
-        */
-        $content = json_decode($response->getContent());
-        return $content;
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function getAllRovers(){
+        $response = $this->httpClient->request('get', 'https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=' . $this->api_key);
+        return json_decode($response->getContent(),true);
     }
+
 }
