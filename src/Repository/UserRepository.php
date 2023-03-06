b7ed4aef579613da44c0f50187fae4b61c996b16
Akhan <93190613+Dulatbay@users.noreply.github.com>
2023-03-06 19:13:25 +0600

    hw_15_symphony

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -56,28 +56,14 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->save($user, true);
     }
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findUsersByUsername(string $username): array
+    {
+        return User::where('username', 'like', '%'.$username.'%')->get()->toArray();
+    }
 
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findNewUsers(\DateTime $date): array
+    {
+        return User::where('created_at', '>', $date)->get()->toArray();
+    }
+
 }
